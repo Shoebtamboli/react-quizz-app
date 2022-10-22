99eda648aa2196a3ab9eec06789a7bf205d0a26b
shoeb <shoebtamboli98@gmail.com>
2022-10-22 23:45:52 +0200

    updated index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
-import * as ReactDOMClient from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = ReactDOMClient.createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 );
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
